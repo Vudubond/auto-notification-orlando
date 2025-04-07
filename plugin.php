9e410fe8f0669ecb6686bd4d7784a47e5df9ecaa
Vu3u <vudubond@gmail.com>
2025-04-07 17:27:27 +0300

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,48 @@
+<?php return array(
+ 'id' => 'Vudubond:auto-notification-orlando', # notrans
+ 'version' => '1.1',
+ 'name' => 'Auto Replyer Orlando',
+ 'author' => 'orlando',
+ 'description' => 'Sends automatic reminders to clients based on ticket status and inactivity.',
+ 'url' => 'https://github.com/Vudubond/auto-notification-orlando/',
+ 'plugin' => 'class.AutoReplyerOrlando.php:AutoReplyerOrlando',
+ 'ost_version' =>    '1.17', # Require osTicket v1.17	
+);
+
+/*
+CHANGELOG
+---------
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [3.1.1] - 2023-01-15
+### Added
+- Changed an $ost->logWarning into $this->LOG[] which I missed earlier.
+
+## [3.1.0] - 2023-01-15
+### Added
+- Consolidate messages and log to osTicket syslog once per run, 
+  instead of multiple times per run.
+
+### Fixed
+- Removed print commands to avoid sending output back to CRON caller.
+
+## [3.0.0] - 2023-01-11
+### Added
+- Compatibility with osTicket 1.17.
+- Multi-instance capability.
+- Send log entries to osTicket syslog instead of to web server's error log (error_log).
+
+### Removed
+- Multiple config options removed from previous version (use instances instead)
+
+## [2.1.0] - 2019-10-13
+### Fixed
+- Forked from original github version.
+
+*/
